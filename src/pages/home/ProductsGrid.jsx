32096478dc10a28914f6d04cbae436af74884516
Velengio Deriksen Charles <velengio@gmail.com>
2026-02-08 23:45:18 +0700

    final data mutation on Home page

--- src/pages/home/ProductsGrid.jsx
+++ src/pages/home/ProductsGrid.jsx
@@ -1,11 +1,9 @@
-import checkmarkDefault from "../../assets/images/icons/checkmark.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { formatMoney } from "../../utils/money";
+import { Product } from "./Product";
 
 export function ProductsGrid({ loadCartItems }) {
   const [products, setProducts] = useState(null);
-  const [quantity, setQuantity] = useState(null);
 
   useEffect(() => {
     const fetchAppData = async () => {
@@ -20,74 +18,13 @@ export function ProductsGrid({ loadCartItems }) {
     fetchAppData();
   }, []);
 
-  async function addToCart(product) {
-    await axios.post('/api/cart-items', {
-      productId: product.id,
-      quantity
-    })
-    await loadCartItems()
-  }
-
-  function selectQuantity(event) {
-    const quantity = Number(event.target.value)
-    setQuantity(quantity)
-  }
-
   return (
     <div className="products-grid">
       {!products ? (
         <div>Loading products...</div>
       ) : (
         products.map((product) => (
-          <div className="product-container" key={product.id}>
-            <div className="product-image-container">
-              <img className="product-image" src={product.image} />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              {product.name}
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src={`images/ratings/rating-${product.rating.stars * 10}.png`}
-              />
-              <div className="product-rating-count link-primary">
-                {product.rating.count}
-              </div>
-            </div>
-
-            <div className="product-price">
-              {formatMoney(product.priceCents)}
-            </div>
-
-            <div className="product-quantity-container">
-              <select value={quantity} onChange={selectQuantity}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src={checkmarkDefault} />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary" onClick={() => addToCart(product)}>
-              Add to Cart
-            </button>
-          </div>
+          <Product product={product} loadCartItems={loadCartItems} key={product.id}/>
         ))
       )}
     </div>
