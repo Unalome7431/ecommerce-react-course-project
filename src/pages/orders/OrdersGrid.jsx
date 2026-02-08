56769d5a0402066ac11c5a4f1c24f2926ab825a9
Velengio Deriksen Charles <velengio@gmail.com>
2026-02-08 17:36:50 +0700

    seperating Order page into more little components

---
+++ src/pages/orders/OrdersGrid.jsx
@@ -0,0 +1,18 @@
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
+import { OrderHeader } from "./OrderHeader";
+
+export function OrdersGrid({ orders }) {
+  return (
+    <div className="orders-grid">
+      {orders.map((order) => {
+        return (
+          <div className="order-container" key={order.id}>
+            <OrderHeader order={order} />
+
+            <OrderDetailsGrid order={order} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
